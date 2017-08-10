aa197fbafa4166c0649814179326a4dc84024db6
解佩旋 <459062059@qq.com>
2017-08-10 23:07:49 +0800

    判断一个数是否为回文数

---
+++ Design-Patterns/src/com/leetcode/ex/_9_is_Palindrome.java
@@ -0,0 +1,23 @@
+package com.leetcode.ex;
+
+public class _9_is_Palindrome {
+
+    public static boolean isPalindrome(int x) {
+
+        if (x < 0 || (x != 0 && x % 10 == 0))
+            return false;
+
+        int rev = 0;
+        while (x > rev) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+
+        return (x == rev) || (x  == rev/10);
+    }
+
+    public static void main(String args[]) {
+        System.out.println(isPalindrome(111));
+    }
+
+}
