a6296108b3b4164af1c068394cd88a0f3224b72a
解佩旋 <peixuan.xie@unspay.com>
2017-09-01 16:54:45 +0800

    412

---
+++ Design-Patterns/src/com/leetcode/ex/_412_Fizz_Buzz.java
@@ -0,0 +1,33 @@
+package com.leetcode.ex;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by peixuan.xie on 2017/9/1.
+ */
+public class _412_Fizz_Buzz {
+
+
+    /*
+    *
+    * Write a program that outputs the string representation of numbers from 1 to n.
+But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+    * */
+
+    public List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                list.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                list.add("Fizz");
+            } else if (i % 5 == 0) {
+                list.add("Buzz");
+            } else {
+                list.add(String.valueOf(i));
+            }
+        }
+        return list;
+    }
+}
