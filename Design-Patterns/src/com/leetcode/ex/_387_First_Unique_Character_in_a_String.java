3f942454d8ae2aa46f1b06b02b76e03b1c3c8bf5
解佩旋 <peixuan.xie@unspay.com>
2017-09-05 15:55:19 +0800

    387. First Unique Character in a String

---
+++ Design-Patterns/src/com/leetcode/ex/_387_First_Unique_Character_in_a_String.java
@@ -0,0 +1,20 @@
+package com.leetcode.ex;
+
+/**
+ * Created by peixuan.xie on 2017/9/5.
+ */
+public class _387_First_Unique_Character_in_a_String {
+
+    public int firstUniqChar(String s) {
+
+        int tmp[] = new int[26];
+
+        for (int i = 0; i < s.length(); i++)
+            tmp[s.charAt(i) - 'a']++;
+
+        for (int i = 0; i < s.length(); i++)
+            if (tmp[s.charAt(i) - 'a'] == 1)
+                return i;
+        return -1;
+    }
+}
